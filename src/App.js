e4f21ad67e62104afe182015e6e5dcab91501e58
Yashar Akish <yashar_akish@yahoo.com>
2022-01-16 18:32:59 -0500

    created POST request functions

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  
+  /**
+   * GET Request
+   */
   const fetchMoviesHandler = useCallback(async () =>{
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-4c059-default-rtdb.firebaseio.com/movies.json');
 
       if (!response.ok) {
         throw new Error('It seems Something went wrong!!!');
@@ -24,15 +26,18 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
     }
@@ -47,9 +52,17 @@ function App() {
   if (movies.length > 0) content = <MoviesList movies={movies} />
   if (error) content= <p>{error}</p>;
   if(isLoading) content= <p>Loading...</p>;
-
-  const addMovieHandler = (movie) => {
-    console.log(movie);
+  /**
+   * POST Request
+   */
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-4c059-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: { 'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
